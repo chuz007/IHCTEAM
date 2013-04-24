fad804addfa19e9377fe239a24e4596199d5f8d1
Cristopher B <chuz007@gmail.com>
2013-04-23 21:36:30 -0600

    New javascript for including self dev plugins

---
+++ js/plugins.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function ErrorHandler(cssClass)
+{    
+    this.errorList = new Array();
+    this.cssCls = cssClass;
+    
+    
+    this.displayMessages = function()
+    {        
+        
+    };
+}
+
+
